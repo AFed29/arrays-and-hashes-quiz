8a023b40f81b6a43ba9ec04868edfd533e05c0f2
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-01-31 15:51:42 +0000

    Section B complete and A added to

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -39,6 +39,15 @@ stops.at(2)
 stops.at(-5)
 stops.fetch(2)
 stops.fetch(-5)
+# stops.each { |stop| p stop if stop == "Falkirk High"}
+
+def find_falkirk_high(array)
+  for stop in array
+    if (stop == "Falkirk High")
+      return stop
+    end
+  end
+end
 
 # 9. Reverse the positions of the stops in the array
 
@@ -46,6 +55,121 @@ stops.reverse!
 
 # 10. Print out all the stops using a for loop
 
-for stop in stops
-  p stop
+# for stop in stops
+#   p stop
+# end
+
+
+
+## B. Given the following data structure:
+
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
+
+
+# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+
+users["Jonathan"][:twitter]
+
+# 2. Get Erik's hometown
+
+users["Erik"][:home_town]
+
+# 3. Get the array of Erik's lottery numbers
+
+users["Erik"][:lottery_numbers]
+
+# 4. Get the type of Avril's pet Monty
+
+users["Avril"][:pets][0][:species]
+
+# 5. Get the smallest of Erik's lottery numbers
+
+users["Erik"][:lottery_numbers].min
+
+# 6. Return an array of Avril's lottery numbers that are even
+
+array = []
+
+for num in users["Avril"][:lottery_numbers]
+  array.push(num) if (num % 2 == 0)
 end
+
+# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+users["Erik"][:lottery_numbers].push(7)
+
+# 8. Change Erik's hometown to Edinburgh
+
+users["Erik"][:home_town] = "Edinburgh"
+
+# 9. Add a pet dog to Erik called "Fluffy"
+
+users["Erik"][:pets].push({name: "fluffy", species: "dog"})
+
+# 10. Add another person to the users hash
+
+ users["Alex"] = {}
+#   twitter: "the__fed",
+#   home_town: "Edinburgh",
+#   pets: [
+#     {
+#       name: "rex",
+#       species: "dog"
+#     }
+#   ]
+# }
